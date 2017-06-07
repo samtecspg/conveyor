7442b3598743b82288cde3934fe75f660b1a65dd
Malavé <luis.malave@gmail.com>
2017-06-07 08:42:52 +0200

    Excluded coverage from server initialization

--- api/index.js
+++ api/index.js
@@ -6,18 +6,17 @@ const Routes = require('./config/routes');
 const Blipp = require('blipp');
 
 module.exports = (callback) => {
-
+    /* $lab:coverage:off$ */
     const server = new Hapi.Server();
     server.connection({ port: process.env.PORT || 80 });
 
-
-    /* $lab:coverage:off$ */
     for (const route in Routes) {
         if (Routes.hasOwnProperty(route)) {
             server.route(Routes[route]);
         }
     }
-    /* $lab:coverage:on$ */
+
     server.register({ register: Blipp, options: {} });
     callback(null, server);
+    /* $lab:coverage:on$ */
 };
